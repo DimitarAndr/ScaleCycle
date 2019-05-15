edc569c51c51ab60dd74539c059e55316a66dddb
Dimitar Andreev <cf17Dimiter.andreev@iesjoandaustria.org>
2019-05-15 13:22:34 +0200

    Adding mapa and premios components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {NavbarComponent} from './navbar/navbar.component';
-
 //Ouxiang Lin
-import { MatDialogModule} from '@angular/material/dialog';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {ClientProfileComponent} from './client-profile/client-profile.component';
 import {LoginComponent} from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import {RegisterComponent} from './register/register.component';
+//Dimitar
 import {FooterComponent} from './footer/footer.component';
 // Dimitar
 import {AgmCoreModule} from '@agm/core';
@@ -24,36 +24,30 @@ import {AgmSnazzyInfoWindowModule} from '@agm/snazzy-info-window';
 import {MapaComponent} from './mapa/mapa.component';
 import {AgmJsMarkerClustererModule} from '@agm/js-marker-clusterer';
 import {FilterPipeModule} from 'ngx-filter-pipe';
-import { PremiosComponent } from './premios/premios.component';
-import { ClientProfileComponent } from './client-profile/client-profile.component';
-import { LoginComponent } from './login/login.component';
-import { FormsModule }   from '@angular/forms';
-import { RegisterComponent } from './register/register.component';
-import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
-import { AdminProfileComponent } from './admin-profile/admin-profile.component';
-import { RegisterSubmitComponent } from './register-submit/register-submit.component';
-import { RegisterChangeComponent } from './register-change/register-change.component';
-import { Globals } from './globals/globals';
-import { ActivateAccountComponent } from './activate-account/activate-account.component';
-import { RecoverPasswordComponent } from './recover-password/recover-password.component';
-
-//Dimitar
-import { FooterComponent } from './footer/footer.component';
+import {PremiosComponent} from './premios/premios.component';
+import {EmployeeProfileComponent} from './employee-profile/employee-profile.component';
+import {AdminProfileComponent} from './admin-profile/admin-profile.component';
+import {RegisterSubmitComponent} from './register-submit/register-submit.component';
+import {RegisterChangeComponent} from './register-change/register-change.component';
+import {Globals} from './globals/globals';
+import {ActivateAccountComponent} from './activate-account/activate-account.component';
+import {RecoverPasswordComponent} from './recover-password/recover-password.component';
 
 
 const rutas = [
-  {path:'', component: HomeComponent},
-  {path:'Client', component: ClientProfileComponent},
-  {path:'RegisterSubmit', component: RegisterSubmitComponent},
-  {path:'RegisterChange', component: RegisterChangeComponent},
-  {path:'Employee', component: EmployeeProfileComponent},
-  {path:'Admin', component: AdminProfileComponent},
-  {path:'Register', component: RegisterComponent},
-  {path:'activateAccount/:id', component: ActivateAccountComponent},
-  {path:'recoverPassword', component: RecoverPasswordComponent},
-  {path:'recoverPassword/:username', component: RecoverPasswordComponent},
+  {path: '', component: HomeComponent},
+  {path: 'Client', component: ClientProfileComponent},
+  {path: 'RegisterSubmit', component: RegisterSubmitComponent},
+  {path: 'RegisterChange', component: RegisterChangeComponent},
+  {path: 'Employee', component: EmployeeProfileComponent},
+  {path: 'Admin', component: AdminProfileComponent},
+  {path: 'Register', component: RegisterComponent},
+  {path: 'activateAccount/:id', component: ActivateAccountComponent},
+  {path: 'recoverPassword', component: RecoverPasswordComponent},
+  {path: 'recoverPassword/:username', component: RecoverPasswordComponent}
   //{path: '**', component: 404}
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
