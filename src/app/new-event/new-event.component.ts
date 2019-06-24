8bf400123bad1fb600942c2ca05fd990dc42243d
DimitarAndreev <dimiter.andreev@iesjoandaustria.org>
2019-06-25 00:34:53 +0200

    new-event: fixing the form in the html and adding idEmpleado in ts. TODO service for adding new event.

--- src/app/new-event/new-event.component.ts
+++ src/app/new-event/new-event.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Globals} from '../globals/globals';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {NgForm} from '@angular/forms';
+import {Evento} from '../model/Evento';
 
 @Component({
   selector: 'app-new-event',
@@ -12,25 +13,26 @@ import {NgForm} from '@angular/forms';
 export class NewEventComponent implements OnInit {
 
 
-  event = {
-    'Titulo': '',
-    'Lugar': '',
-    'Descripcion': '',
-    'Puntos': '',
-    'Fecha': '',
-    'HoraInicio': '',
-    'HoraFinal': '',
-    'Id_empleado': ''
-  }
+  event: Evento;
+
 
   constructor(private http: HttpClient, private globals: Globals, private router: Router) {
   }
 
+  @ViewChild('newEvent')
+  newEvent: NgForm;
+
   ngOnInit() {
+
   }
 
   onSubmit() {
-    this.event['Id_empleado'] = JSON.parse(localStorage.getItem('user'))['userId'];
+    const idEmpleado = JSON.parse(localStorage.getItem('user'))[0]._id;
+    this.event = this.newEvent.value;
+    this.event.idEmpleado = idEmpleado;
+
+   //TODO
+    /*Need to move adding new event to the service evento.service.ts */
 
     this.http.post(this.globals['SERVER'] + '/newEvnet', this.event).subscribe(data => {
       if (data['error']) {
@@ -42,7 +44,7 @@ export class NewEventComponent implements OnInit {
     });
   }
 
-/*  onFileChange(event) {
+  /*  onFileChange(event) {
 
-  }*/
+    }*/
 }
